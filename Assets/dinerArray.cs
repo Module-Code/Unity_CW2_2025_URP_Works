069bee2e91ad52f12ba40a75de446f15ed81cae7
XRLabLondon <44606355+XRLabLondon@users.noreply.github.com>
2024-09-04 16:38:38 +0100

    Initial commit

---
+++ Assets/dinerArray.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dinerArray : MonoBehaviour
+{
+    public GameObject[] dinerListArray;
+    public GameObject[] dinerSpawnList;
+    //public int dinerIndex;
+    //public int dinerTotal;
+    //public GameObject selectedDiner;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
